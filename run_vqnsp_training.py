09e5f9419287efb68e940696cfe496f79a2bc20d
gandrew13 <andrei.galbenus13@gmail.com>
2024-11-07 11:11:24 +0100

    Removed some distributed running logic to simplify the code. Adapting to the Alljoined1 dataset (WIP).

--- run_vqnsp_training.py
+++ run_vqnsp_training.py
@@ -26,6 +26,7 @@ from engine_for_vqnsp import evaluate, train_one_epoch, calculate_codebook_usage
 from utils import NativeScalerWithGradNormCount as NativeScaler
 import modeling_vqnsp
 import utils
+from dataset_maker.alljoined1 import load_alljoined1_dataset
 
 
 def get_args():
@@ -144,90 +145,31 @@ def main(args):
 
     model = get_model(args)
 
-    # get dataset
-    # datasets with the same montage can be packed within a sublist
-    datasets_train = [
-        ["path/to/dataset1", "path/to/dataset2"], # e.g., 64 channels for dataset1 and dataset2
-        ["path/to/dataset3", "path/to/dataset4"], # e.g., 32 channels for dataset3 and dataset4
-    ]
     # time window for each sublist in dataset_train
     # to ensure the total sequence length be around 256 for each dataset
     time_window = [
         4, # set the time window to 4 so that the sequence length is 4 * 64 = 256
         8, # set the time window to 8 so that the sequence length is 8 * 32 = 256
     ]
-    dataset_train_list, train_ch_names_list = utils.build_pretraining_dataset(datasets_train, time_window, stride_size=200)
+    #dataset_train_list, train_ch_names_list = utils.build_pretraining_dataset(datasets_train, time_window, stride_size=200)
+    
+    args.multi_label = False
+    train_ds, _, test_ds = load_alljoined1_dataset(args, time_window, stride_size=200)
 
-    datasets_val = [
-        ["path/to/datasets_val"]
-    ]
-    if args.disable_eval:
-        dataset_val_list = None
-    else:
-        dataset_val_list, val_ch_names_list = utils.build_pretraining_dataset(datasets_val, [4])
+    sampler_train = torch.utils.data.RandomSampler(train_ds)
+    sampler_val = torch.utils.data.SequentialSampler(test_ds)
 
-    if True:  # args.distributed:
-        num_tasks = utils.get_world_size()
-        global_rank = utils.get_rank()
-        sampler_rank = global_rank
-        num_training_steps_per_epoch = sum([len(dataset) for dataset in dataset_train_list]) // args.batch_size // num_tasks
-
-        sampler_train_list = []
-        for dataset in dataset_train_list:
-            sampler_train = torch.utils.data.DistributedSampler(
-                dataset, num_replicas=num_tasks, rank=sampler_rank, shuffle=True
-            )
-            sampler_train_list.append(sampler_train)
-
-        print("Sampler_train = %s" % str(sampler_train))
-        sampler_eval_list = []
-        if args.dist_eval:
-            # if len(dataset_val) % num_tasks != 0:
-            #     print('Warning: Enabling distributed evaluation with an eval dataset not divisible by process number. '
-            #           'This will slightly alter validation results as extra duplicate entries are added to achieve '
-            #           'equal num of samples per-process.')
-            for dataset in dataset_val_list:
-                sampler_val = torch.utils.data.DistributedSampler(
-                    dataset, num_replicas=num_tasks, rank=global_rank, shuffle=False)
-                sampler_eval_list.append(sampler_val)
-        else:
-            for dataset in dataset_val_list:
-                sampler_val = torch.utils.data.SequentialSampler(dataset)
-                sampler_eval_list.append(sampler_val)
-    else:
-        sampler_train = torch.utils.data.RandomSampler(dataset_train)
-        sampler_val = torch.utils.data.SequentialSampler(dataset_val)
-
+    global_rank = 0
     if global_rank == 0 and args.log_dir is not None:
         os.makedirs(args.log_dir, exist_ok=True)
         log_writer = utils.TensorboardLogger(log_dir=args.log_dir)
     else:
         log_writer = None
 
-    data_loader_train_list = []
-    for dataset, sampler in zip(dataset_train_list, sampler_train_list):
-        data_loader_train = torch.utils.data.DataLoader(
-            dataset, sampler=sampler,
-            batch_size=args.batch_size,
-            num_workers=args.num_workers,
-            pin_memory=args.pin_mem,
-            drop_last=True,
-        )
-        data_loader_train_list.append(data_loader_train)
+    data_loader_train = torch.utils.data.DataLoader(train_ds, sampler=sampler_train, batch_size=args.batch_size, num_workers=args.num_workers, pin_memory=args.pin_mem, drop_last=True)
+    data_loader_val = torch.utils.data.DataLoader(test_ds, sampler=sampler_val, batch_size=int(1.5 * args.batch_size), num_workers=args.num_workers, pin_memory=args.pin_mem, drop_last=False)
 
-    if dataset_val_list is not None:
-        data_loader_val_list = []
-        for dataset, sampler in zip(dataset_val_list, sampler_eval_list):
-            data_loader_val = torch.utils.data.DataLoader(
-                dataset, sampler=sampler,
-                batch_size=int(1.5 * args.batch_size),
-                num_workers=args.num_workers,
-                pin_memory=args.pin_mem,
-                drop_last=False
-            )
-            data_loader_val_list.append(data_loader_val)
-    else:
-        data_loader_val_list = None
+    num_training_steps_per_epoch = len(train_ds) // args.batch_size // 1
 
     model.to(device)
     model_without_ddp = model
@@ -257,10 +199,6 @@ def main(args):
     optimizer = create_optimizer(args, model_without_ddp)
     loss_scaler = NativeScaler()
 
-    if args.distributed:
-        model = torch.nn.parallel.DistributedDataParallel(model, device_ids=[args.gpu], find_unused_parameters=True)
-        model_without_ddp = model.module
-
     print("Use step level LR & WD scheduler!")
     lr_schedule_values = utils.cosine_scheduler(
         args.lr, args.min_lr, args.epochs, num_training_steps_per_epoch,
@@ -282,34 +220,21 @@ def main(args):
     start_time = time.time()
             
     for epoch in range(args.start_epoch, args.epochs):
-        if args.distributed:
-            for data_loader_train in data_loader_train_list:
-                data_loader_train.sampler.set_epoch(epoch)
         if log_writer is not None:
             log_writer.set_step(epoch * num_training_steps_per_epoch)
+        
         train_stats = train_one_epoch(
-            model, 
-            data_loader_train_list,
-            optimizer, 
-            device, 
-            epoch, 
-            loss_scaler,
-            args.clip_grad, 
-            log_writer=log_writer,
-            start_steps=epoch * num_training_steps_per_epoch,
-            lr_schedule_values=lr_schedule_values,
-            ch_names_list=train_ch_names_list,
-            args=args
-        )
+            model, [data_loader_train,], optimizer, device, epoch, loss_scaler, args.clip_grad, log_writer=log_writer, start_steps=epoch * num_training_steps_per_epoch, lr_schedule_values=lr_schedule_values,
+            ch_names_list=[["O1", "OZ", "O2", "PO7", "PO3", "POZ", "PO4", "PO8"],], args=args)
+        
         if args.output_dir:
             # if (epoch + 1) % args.save_ckpt_freq == 0 or epoch + 1 == args.epochs:
             utils.save_model(
                 args=args, model=model, model_without_ddp=model_without_ddp, optimizer=optimizer,
                 loss_scaler=loss_scaler, epoch=epoch, save_ckpt_freq=args.save_ckpt_freq)
         
-        if data_loader_val_list is not None:
-            test_stats = evaluate(data_loader_val_list, model, device, log_writer, epoch, ch_names_list=val_ch_names_list, args=args)
-            print(f"Validation loss of the network on the {sum([len(dataset) for dataset in dataset_val_list])} test EEG: {test_stats['loss']:.4f}")
+            test_stats = evaluate([data_loader_val], model, device, log_writer, epoch, ch_names_list=val_ch_names_list, args=args)
+            print(f"Validation loss of the network on the {sum([len(dataset) for dataset in [data_loader_val]])} test EEG: {test_stats['loss']:.4f}")
 
             if log_writer is not None:
                 log_writer.update(**test_stats, head="val/loss")
